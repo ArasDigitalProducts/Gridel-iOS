96d206d31fa17f84f97b01d71cb34e3b6d25512a
Marko Benacic <34174145+Markobenacic@users.noreply.github.com>
2023-04-21 17:17:31 +0200

    test UIWindow

--- Sources/Gridel/Gridel.swift
+++ Sources/Gridel/Gridel.swift
@@ -5,8 +5,34 @@ public struct Gridel {
 
 //    private var window: UIWindow?
 
+    public static var configStyle: ConfigStyle = .simple
+
     public init() {
+
+    }
+
+    public static func configure(style: ConfigStyle) {
+        configStyle = style
+    }
+
+    public static func applyGrid() {
+        guard let keyWindow = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+
+        switch configStyle {
+        case .simple:
+            keyWindow.window?.rootViewController?.view.backgroundColor = .green
+        case .verbose:
+            keyWindow.window?.rootViewController?.view.backgroundColor = .orange
+        }
+
     }
 
 
+
+
+}
+
+public enum ConfigStyle {
+    case simple
+    case verbose
 }
