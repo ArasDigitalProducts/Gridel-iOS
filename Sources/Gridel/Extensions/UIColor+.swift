03b764613fc810798a68b3446108a7fc1e8c6fed
Marko Benacic <34174145+Markobenacic@users.noreply.github.com>
2023-05-15 16:52:51 +0200

    add colors resources

---
+++ Sources/Gridel/Extensions/UIColor+.swift
@@ -0,0 +1,28 @@
+//
+//  UIColor+.swift
+//  
+//
+//  Created by Marko Benačić on 15.05.2023..
+//
+
+import UIKit
+
+extension UIColor {
+
+    static var background: UIColor {
+        return UIColor(named: "background") ?? .red
+    }
+
+    static var primary1: UIColor {
+        return UIColor(named: "primary1") ?? .red
+    }
+
+    static var primary2: UIColor {
+        return UIColor(named: "primary2") ?? .red
+    }
+
+    static var secondary1: UIColor {
+        return UIColor(named: "secondary1") ?? .red
+    }
+
+}
