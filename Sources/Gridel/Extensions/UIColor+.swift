cde2d26869daac26a2da0264711d760ab6448ae9
Marko Benacic <34174145+Markobenacic@users.noreply.github.com>
2023-05-17 13:54:27 +0200

    add colors

--- Sources/Gridel/Extensions/UIColor+.swift
+++ Sources/Gridel/Extensions/UIColor+.swift
@@ -9,20 +9,38 @@ import UIKit
 
 extension UIColor {
 
-    static var background: UIColor {
-        return UIColor(named: "background") ?? .red
+    static var blackBackground: UIColor {
+        return UIColor(named: "blackBackground") ?? .red
     }
 
-    static var primary1: UIColor {
-        return UIColor(named: "primary1") ?? .red
+    static var p50: UIColor {
+        return UIColor(named: "P50") ?? .red
     }
 
-    static var primary2: UIColor {
-        return UIColor(named: "primary2") ?? .red
+    static var p75: UIColor {
+        return UIColor(named: "P75") ?? .red
     }
 
-    static var secondary1: UIColor {
-        return UIColor(named: "secondary1") ?? .red
+    static var p100: UIColor {
+        return UIColor(named: "P100") ?? .red
     }
+
+    static var p200: UIColor {
+        return UIColor(named: "P200") ?? .red
+    }
+
+    static var p300: UIColor {
+        return UIColor(named: "P300") ?? .red
+    }
+
+    static var g400: UIColor {
+        return UIColor(named: "G400") ?? .red
+    }
+
+    static var g500: UIColor {
+        return UIColor(named: "G500") ?? .red
+    }
+    
+
 
 }
