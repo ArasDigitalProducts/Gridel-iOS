b035c4cbc11fdde41ea08a2a1a3614e0bafa12b1
Marko Benacic <34174145+Markobenacic@users.noreply.github.com>
2023-05-22 16:01:37 +0200

    add container view

--- Sources/Gridel/SettingsViewController.swift
+++ Sources/Gridel/SettingsViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    var containerView: UIView!
+
     var cancelButton: UIBarButtonItem!
     var saveButton: UIBarButtonItem!
+    var optionSegmentedControl: UISegmentedControl!
 
     override func viewDidLoad() {
         view.backgroundColor = .white
         setupHideKeyboardOnTap()
 
+        initViews()
         setupUI()
         renderViews()
     }
@@ -24,24 +28,60 @@ class SettingsViewController: UIViewController {
         Gridel.gridelWindow?.isUserInteractionEnabled = false
     }
 
+    private func initViews() {
+        cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(leftButtonAction))
+        saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(rightButtonAction))
+        optionSegmentedControl = UISegmentedControl(items: ["Columns", "Rows"])
+
+        containerView = UIView()
+
+    }
+
     private func setupUI() {
         view.backgroundColor = .blackBackground
 
         navigationItem.title = "Gridel"
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
 
-        cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(leftButtonAction))
-        saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(rightButtonAction))
-
         navigationItem.leftBarButtonItem = cancelButton
         cancelButton.tintColor = .p300
 
         navigationItem.rightBarButtonItem = saveButton
         saveButton.tintColor = .p300
+
+        optionSegmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
     }
 
     private func renderViews() {
+        view.addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.topAnchor.constraint(equalTo: view.topAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        containerView.backgroundColor = .orange
 
+    }
+
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            rowsSelected()
+        case 1:
+            columnsSelected()
+        default:
+            break
+        }
+    }
+
+    private func rowsSelected() {
+        print("rows")
+    }
+
+    private func columnsSelected() {
+        print("columns")
     }
 
     @objc func leftButtonAction() {
